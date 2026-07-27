dc5f3b1b3cbc2244ac87efb9dce2d12789db80f4
MoatazAdel317 <MoatazAdel317@gmail.com>
2026-07-27 15:08:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const click = document.querySelector(".btn");
+click.addEventListener("click",function c (){alert("You Are Loged In")});
+
+
+const com = document.querySelector(".for");
+com.addEventListener("click",function co (){alert("Coming Soon . . .")});
+
+
+const com2 = document.querySelector(".for2");
+com2.addEventListener("click",function coo (){alert("Coming Soon Too . . .")});
